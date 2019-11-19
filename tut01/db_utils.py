abe738d9e92d79a1e62766a18228c1469b95f671
gotfree <deathbydissection@gmail.com>
2019-11-19 11:49:44 +0400

    create utility script --FW

--- tut01/db_utils.py
+++ tut01/db_utils.py
@@ -1,4 +1,9 @@
 import os
 import sqlite3
 
-DEFAULT_PATH = os.path.join(os.path)
+DEFAULT_PATH = os.path.join(os.path.dirname(__file__), 'db.sqlite3')
+
+
+def db_connect(db_path=DEFAULT_PATH):
+    con = sqlite3.connect(db_path)
+    return con
