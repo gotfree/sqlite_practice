e3c4da63f6bf214cff0e5c902821a72b0619b20e
Rob Kano <deathbydissection@gmail.com>
2019-11-21 22:49:30 +0400

    restruct all --FH

--- tut01/db_utils.py
+++
@@ -1,9 +0,0 @@
-import os
-import sqlite3
-
-DEFAULT_PATH = os.path.join(os.path.dirname(__file__), 'db.sqlite3')
-
-
-def db_connect(db_path=DEFAULT_PATH):
-    con = sqlite3.connect(db_path)
-    return con
--- tut01/init_db.py
+++ tut01/init_db.py
@@ -1,41 +1,73 @@
-from db.db_utils import db_connect
+import os
+import sqlite3
+from sqlite3 import Error
 
-con = db_connect()
-cur = con.cursor()
 
-customers_sql = """
-CREATE TABLE customers (
-    id integer PRIMARY KEY,
-    first_name text NOT NULL,
-    last_name text NOT NULL
-)"""
-cur.execute(customers_sql)
+def db_connect(db_path):
+    con = None
+    try:
+        con = sqlite3.connect(db_path)
+        print(f"New connection created successfully, sqlite3 version: {sqlite3.version}")
+        return con
+    except Error as e:
+        print(e)
+    return con
 
-products_sql = """
-CREATE TABLE products (
-    id integer PRIMARY KEY,
-    name text NOT NULL,
-    price real NOT NULL
-)"""
-cur.execute(products_sql)
 
-orders_sql = """
-CREATE TABLE orders (
-    id integer PRIMARY KEY,
-    date text NOT NULL,
-    customer_id integer,
-    FOREIGN KEY (customer_id) REFERENCES customers (id)
-)"""
-cur.execute(orders_sql)
+def create_table(con, create_table_sql):
+    try:
+        c = con.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
 
-lineitems_sql = """
-CREATE TABLE lineitems (
-    id integer PRIMARY KEY,
-    quantity integer NOT NULL,
-    total real NOT NULL,
-    product_id integer,
-    order_id integer,
-    FOREIGN KEY (product_id) REFERENCES products (id),
-    FOREIGN KEY (order_id) REFERENCES orders (id)
-)"""
-cur.execute(lineitems_sql)
+
+def main():
+    DEFAULT_PATH = os.path.join(os.path.dirname(__file__), 'db.sqlite3')
+
+    customers_sql = """
+    CREATE TABLE IF NOT EXISTS customers (
+        id integer PRIMARY KEY,
+        first_name text NOT NULL,
+        last_name text NOT NULL
+    );"""
+
+    products_sql = """
+    CREATE TABLE IF NOT EXISTS products (
+        id integer PRIMARY KEY,
+        name text NOT NULL,
+        price real NOT NULL
+    );"""
+
+    orders_sql = """
+    CREATE TABLE IF NOT EXISTS orders (
+        id integer PRIMARY KEY,
+        date text NOT NULL,
+        customer_id integer,
+        FOREIGN KEY (customer_id) REFERENCES customers (id)
+    );"""
+
+    lineitems_sql = """
+    CREATE TABLE IF NOT EXISTS lineitems (
+        id integer PRIMARY KEY,
+        quantity integer NOT NULL,
+        total real NOT NULL,
+        product_id integer,
+        order_id integer,
+        FOREIGN KEY (product_id) REFERENCES products (id),
+        FOREIGN KEY (order_id) REFERENCES orders (id)
+    );"""
+
+    con = db_connect(DEFAULT_PATH)
+
+    if con is not None:
+        create_table(con, customers_sql)
+        create_table(con, products_sql)
+        create_table(con, orders_sql)
+        create_table(con, lineitems_sql)
+    else:
+        print("Error! cannot create db connection.")
+
+
+if __name__ == '__main__':
+    main()
